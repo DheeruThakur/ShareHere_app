0da09bdfec8f1a9dbd4009fc39e32a280268ae64
dheeru <sdheerendra382@gmail.com>
2022-03-01 00:07:12 +0530

    error setup

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Method", "POST , GET");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type , Authorization");
 
   next();
 });
